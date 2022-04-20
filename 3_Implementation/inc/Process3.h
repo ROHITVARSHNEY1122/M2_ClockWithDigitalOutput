6034927a61ae44694aabdeea31522f6a167543eb
ROHIT VARSHNEY <varshneyrohit1122@gmail.com>
2022-04-20 08:31:23 +0530

    Update Process3.h

--- 3_Implementation/inc/Process3.h
+++ 3_Implementation/inc/Process3.h
@@ -1,3 +1,9 @@
+/**
+ * @file Process3.h
+ * @author Rohit Varshney
+ * @brief Header file for ClockWithDigitalOutput
+ *
+ */
 #ifndef PROCESS3_H_INCLUDED
 #define PROCESS3_H_INCLUDED
 void HoursToCalculate(bool display_time);
