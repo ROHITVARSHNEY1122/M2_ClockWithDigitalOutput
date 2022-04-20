136365758342d6e84f69b69db157b18b10b5345b
ROHIT VARSHNEY <varshneyrohit1122@gmail.com>
2022-04-20 08:29:00 +0530

    Update Process1.h

--- 3_Implementation/inc/Process1.h
+++ 3_Implementation/inc/Process1.h
@@ -1,3 +1,9 @@
+/**
+ * @file Process1.h
+ * @author Rohit Varshney
+ * @brief Header file for ClockWithDigitalOutput
+ *
+ */
 #ifndef PROCESS1_H_INCLUDED
 #define PROCESS1_H_INCLUDED
 void SecondsToCalculate(bool display_time);
