ea5d0fb39e0438fbbd25dd65826905d3b365d10b
ROHIT VARSHNEY <varshneyrohit1122@gmail.com>
2022-04-20 08:34:05 +0530

    Update Process4.h

--- 3_Implementation/inc/Process4.h
+++ 3_Implementation/inc/Process4.h
@@ -1,3 +1,9 @@
+/**
+ * @file Process4.h
+ * @author Rohit Varshney
+ * @brief Header file for ClockWithDigitalOutput
+ *
+ */
 #ifndef PROCESS4_H_INCLUDED
 #define PROCESS4_H_INCLUDED
 void Displayprinting(uint8_t DIGIT, uint8_t NUMBERDISPLAY);
