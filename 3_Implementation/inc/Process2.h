df92ff1c966aacbd64eba9cfd48c91ece5672fee
ROHIT VARSHNEY <varshneyrohit1122@gmail.com>
2022-04-20 08:29:54 +0530

    Update Process2.h

--- 3_Implementation/inc/Process2.h
+++ 3_Implementation/inc/Process2.h
@@ -1,3 +1,9 @@
+/**
+ * @file Process2.h
+ * @author Rohit Varshney
+ * @brief Header file for ClockWithDigitalOutput
+ *
+ */
 #ifndef PROCESS2_H_INCLUDED
 #define PROCESS2_H_INCLUDED
 void MinutesToCalculate(bool display_time);
